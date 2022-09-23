97e4baf6046825696373b4d0c8cfea260d9d0d98
Camilo Andrade <andradecamilo226@gmail.com>
2022-09-23 17:25:40 -0500

    diploy

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ const app = express();
 
 app.use(express.static(panth.join(__dirname, 'public')));
 
-app.listen(3001,function(){
+/*app.listen(3001,function(){
     console.log('run server');
-});
+});*/
 
 app.get('/' , (req, res) =>{
     res.sendFile(panth.join(__dirname, './views/index.html'))
-})
+});
+
+app.listen(process.env.PORT || 3000, function (){
+    console.log('run server');
+});
