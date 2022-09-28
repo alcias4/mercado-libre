c48317dbbae6504b9176a72f85059fcb4dc2ac45
Camilo Andrade <andradecamilo226@gmail.com>
2022-09-27 21:09:09 -0500

    mejoras front

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const panth = require('path');
 
 const app = express();
 
-app.use(express.static(panth.join(__dirname, 'public')));
 
-/*app.listen(3001,function(){
-    console.log('run server');
+/*app.listen(3001, function(){
+    console.log('run server')
 });*/
+
+
+app.use(express.static(panth.join(__dirname, 'public')));
 
 app.get('/' , (req, res) =>{
     res.sendFile(panth.join(__dirname, './views/index.html'))
